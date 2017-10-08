48fa35fdd8db1ead0d1e51c8d7f76d71c2cbc38f
perplexinglysimple <boyjorge@vt.edu>
2017-10-08 16:12:32 -0400

    Code to dump and reset the eeprom

---
+++ October_Launch/ResetEEPROM/ResetEEPROM.ino
@@ -0,0 +1,96 @@
+
+//This program resets or dumps the output of the eeprom to Serial.
+
+//Define RESET to reset the chip. Comment-out to dump the contents
+#define RESET
+
+#ifndef RESET
+#define OUT
+#endif
+
+#define MEMORY_SIZE 0x2000 //total bytes can be accessed 24LC64 = 0x2000 (maximum address = 0x1FFF)
+#include <Wire.h>    
+ 
+#define disk1 0x50    //Address of eeprom chip
+
+#ifdef RESET
+void setup(void)
+{
+  Serial.begin(9600);
+  Wire.begin();  
+  Serial.print(readEEPROM(disk1, 0), HEX);
+  Serial.println();
+  unsigned int address = 0;
+  writeEEPROM(disk1, 0, 3);
+  writeEEPROM(disk1, 1, 0);
+  writeEEPROM(disk1, 2, 0);
+  Serial.print(readEEPROM(disk1, 0), HEX);
+  Serial.print(readEEPROM(disk1, 1), HEX);
+  Serial.print(readEEPROM(disk1, 2), HEX);
+  Serial.println();
+
+}
+#endif
+
+#ifdef OUT
+void setup(void)
+{
+  Serial.begin(9600);
+  Wire.begin();  
+  Serial.print("Start In");
+  delay(1000);
+  Serial.print(3);
+  delay(1000);
+  Serial.print(2);
+  delay(1000);
+  Serial.print(1);
+  Serial.println();
+  Serial.print(readEEPROM(disk1, 0), HEX);
+  Serial.print(readEEPROM(disk1, 1), HEX);
+  Serial.print(readEEPROM(disk1, 2), HEX);
+  Serial.println();
+  unsigned int address = 0;
+  for(size_t i = 3; i < MEMORY_SIZE; i++)
+  {
+    Serial.print(readEEPROM(disk1, i), HEX);
+    Serial.print(:);
+    Serial.print(readEEPROM(disk1, i), HEX);
+    Serial.print(readEEPROM(disk1, i), HEX);
+    Serial.print(readEEPROM(disk1, i), HEX);
+    Serial.print(readEEPROM(disk1, i), HEX);
+    Serial.println();
+  }
+  Serial.println();
+
+}
+#endif
+
+
+void loop(){}
+ 
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) 
+{
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+ 
+  delay(5);
+}
+ 
+byte readEEPROM(int deviceaddress, unsigned int eeaddress ) 
+{
+  byte rdata = 0xFF;
+ 
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.endTransmission();
+ 
+  Wire.requestFrom(deviceaddress,1);
+ 
+  if (Wire.available()) rdata = Wire.read();
+ 
+  return rdata;
+}
